9626d2bfa88e219b82948b61b7444f93b5572407
iiMaInUsErii <hromovartur94@gmail.com>
2024-04-17 10:25:41 +0300

    cros and /delete

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, render_template, session, jsonify
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import sqlite3
 import time
 
 app = Flask(__name__)
 app.secret_key = b'fw!pk4[2f4%#g&bh".<t'
 CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 dbconn = sqlite3.connect('db.db', check_same_thread=False)
 cursor = dbconn.cursor()
 
@@ -25,10 +26,12 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS messages (
 )''')
 
 @app.route("/")
+@cross_origin()
 def index():
     return jsonify(message='Server working')
 
-@app.route("/chat", methods=['JSON', 'POST', 'GET', 'SESSION'])
+@app.route("/chat", methods=['POST', 'GET', 'SESSION'])
+@cross_origin()
 def chat():
     if 'name' in request.json:
         name = request.json['name']
@@ -79,21 +82,53 @@ def chat():
             )
 
 @app.route('/new', methods=['POST'])
+@cross_origin()
 def add():
-    cursor.execute("INSERT INTO messages (room, name, text, time) VALUES (?, ?, ?, ?)",
-                   (session['room'], session['name'], request.json['text'], int(time.time())))
-    dbconn.commit()
+    room = request.json['room']
+    name = request.json['name']
+    password = request.json['password']
+    text = request.json['text']
 
+    cursor.execute("SELECT * FROM rooms WHERE name = ?", [room])
+    if cursor.fetchall()[0][2] == password:
+        cursor.execute("INSERT INTO messages (room, name, text, time) VALUES (?, ?, ?, ?)", (room, name, text, int(time.time())))
+        dbconn.commit()
+
+        return jsonify(
+            status='seccessful'
+        )
     return jsonify(
-        status='seccessful'
+        status='error'
+    )
+
+@app.route('/delete', methods=['POST'])
+@cross_origin()
+def delete():
+    room = request.json['room']
+    name = request.json['name']
+    password = request.json['password']
+    id = request.json['id']
+
+    cursor.execute("SELECT * FROM rooms WHERE name = ? ", [room])
+    if cursor.fetchall()[0][2] == password:
+        cursor.execute("DELETE FROM messages WHERE id = ? and name = ?", (id, name))
+        dbconn.commit()
+
+        return jsonify(
+            status='seccessful'
+        )
+    return jsonify(
+        status='error'
     )
 
 @app.route('/logout')
+@cross_origin()
 def logout():
     session.clear()
     return index()
 
 @app.route('/admin', methods=['GET'])
+@cross_origin()
 def admin():
     if request.args.get("command"):
         command = request.args.get("command")
